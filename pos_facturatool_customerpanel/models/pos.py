132d47ba976f0ce1e3858ccaf0efb4ed12d9b59e
vehosoft <frodriguez@vehosoft.com>
2021-11-01 05:15:24 +0000

    update pos_facturatool_customerpanel version 1.1

--- pos_facturatool_customerpanel/models/pos.py
+++ pos_facturatool_customerpanel/models/pos.py
@@ -38,6 +38,114 @@ class PosOrder(models.Model):
         _logger.debug('===== _order_fields vals = %r',vals)
         return vals
 
+    def action_send_order_to_facturatool(self):
+        wsdl = 'http://ws.facturatool.com/index.php?wsdl'
+        client = zeep.Client(wsdl)
+        for pos_order in self:
+            ft_account = self.env['facturatool.account'].search([('rfc','!=',''),('company_id','=',pos_order.company_id.id)], limit=1)
+            if ft_account.rfc != False:
+                _logger.debug('===== action_send_order_to_facturatool pos_order.name = %r',pos_order.name)
+                
+                conceptos = []
+                taxes = []
+                iline = 0
+                itax = 0
+                discount=0.00
+                for line in pos_order.lines:
+                    _logger.debug('===== action_send_order_to_facturatool line.display_name = %r',line.display_name)
+                    _logger.debug('===== action_send_order_to_facturatool line.tax_ids = %r',line.tax_ids)
+                    _logger.debug('===== action_send_order_to_facturatool line.tax_ids_after_fiscal_position = %r',line.tax_ids_after_fiscal_position)
+                    tax_include_on_price = False
+                    for tax in line.tax_ids:
+                        factor_tax = 1.00
+                        if tax.amount < 0.00:
+                            factor_tax = -1.00
+                        if tax.amount_type=='percent' and tax.type_tax_sat != False:
+                            tax_obj = {
+                                'Nombre': tax.name,
+                                'Tipo': tax.type_tax_sat,
+                                'Impuesto': tax.clave_sat,
+                                'Base': line.price_subtotal,
+                                'TipoFactor': tax.tipo_factor_sat,
+                                'TasaOCuota': (float(tax.amount)/100.00) * factor_tax,
+                                'Importe': float(line.price_subtotal) * (float(tax.amount)/100.00) * factor_tax,
+                                'indexConcepto': iline
+                            }
+                            taxes[itax:itax]=[tax_obj]
+                        itax = itax + 1
+                        if tax.price_include:
+                            tax_include_on_price = True
+                    ValorUnitario = line.price_unit
+                    if tax_include_on_price:
+                        ValorUnitario = float(line.price_subtotal) / float(line.qty)
+                    
+                    concepto = {
+                        'ClaveProdServ': line.product_id.clave_sat.code,
+                        'Cantidad': line.qty,
+                        'Descripcion': line.display_name,
+                        'ClaveUnidad': line.product_uom_id.clave_sat,
+                        'Unidad': line.product_uom_id.name,
+                        'ValorUnitario': ValorUnitario,
+                        'Importe': line.price_subtotal,
+                        'indexConcepto': iline
+                    }
+                    
+                    #if line.discount > 0:
+                    #    discount = discount + line.discount
+                    #    concepto['Descuento'] = line.discount
+
+                    conceptos[iline:iline]=[concepto]
+                    iline = iline + 1
+                
+                #Obtener la forma de pago
+                cfdi_forma_pago = '99'
+                payment_amount = 0.00
+                for payment in pos_order.payment_ids:
+                    if payment.amount > payment_amount:
+                        cfdi_forma_pago = payment.payment_method_id.cfdi_forma_pago.code or cfdi_forma_pago
+                        payment_amount = payment.amount
+                
+                params = {
+                    'Rfc': ft_account.rfc,
+                    'Usuario': ft_account.username,
+                    'Password': ft_account.password,
+                    'Fecha': pos_order.date_order.strftime("%Y-%m-%d %H:%M"),
+                    'Codigo': pos_order.cfdi_ticket_codigo,
+                    'FormaPago': cfdi_forma_pago,
+                    'Conceptos': conceptos,
+                    'Moneda': 'MXN',#pos_order.currency_id.name, hasta que se implemente timbrado para monedas distintas a MXN
+                    'SubTotal': float(pos_order.amount_total) - float(pos_order.amount_tax),
+                    'Total': pos_order.amount_total,
+                    'LugarExpedicion': pos_order.company_id.zip,
+                    'IdExterno': pos_order.name+'_'+str(pos_order.id),
+                }
+                #if discount > 0.00:
+                #    params['Descuento'] =  discount
+                if len(taxes) > 0:
+                    params['Impuestos'] =  taxes
+                
+                _logger.debug('===== action_send_order_to_facturatool crearTicket params = %r',params)
+                result = client.service.crearTicket(params=params)
+                _logger.debug('===== action_send_order_to_facturatool crearTicket result = %r',result)
+                ws_res = json.loads(result)
+                _logger.debug('===== action_send_order_to_facturatool crearTicket ws_res = %r',ws_res)
+
+                if ws_res['success'] == True:
+                    pos_order.write({
+                        'cfdi_ticket_state':'sync',
+                        'cfdi_ticket_id':ws_res['trans_id'],
+                        'cfdi_ticket_calls': pos_order.cfdi_ticket_calls + 1,
+                    })
+                else:
+                    if ws_res['error'] is None:
+                        error = "Servicio temporalmente fuera de servicio"
+                    else:
+                        error = 'Error #' + str(ws_res['errno']) + ': ' + ws_res['error']
+                    pos_order.write({
+                        'cfdi_ticket_calls': pos_order.cfdi_ticket_calls + 1,
+                        'cfdi_ticket_call_error': error
+                    })
+
     def cron_send_pos_orders_to_facturatool(self):
         wsdl = 'http://ws.facturatool.com/index.php?wsdl'
         client = zeep.Client(wsdl)
@@ -49,7 +157,7 @@ class PosOrder(models.Model):
         last_order_id = cron_obj.facturatool_count
         _logger.debug('===== cron_send_pos_orders_to_facturatool last_order_id = %r',last_order_id)
 
-        pos_orders = self.env['pos.order'].search([('id','>',last_order_id),('cfdi_ticket_codigo','!=',''),('cfdi_ticket_state','=','pending')], limit=30)
+        pos_orders = self.env['pos.order'].search([('id','>',last_order_id),('cfdi_ticket_codigo','!=',''),('cfdi_ticket_state','=','pending')], limit=30, order='id asc')
         _logger.debug('===== cron_send_pos_orders_to_facturatool len(pos_orders) = %r',len(pos_orders))
 
         for pos_order in pos_orders:
@@ -109,9 +217,9 @@ class PosOrder(models.Model):
                         'indexConcepto': iline
                     }
                     
-                    if line.discount > 0:
-                        discount = discount + line.discount
-                        concepto['Descuento'] = line.discount
+                    #if line.discount > 0:
+                    #    discount = discount + line.discount
+                    #    concepto['Descuento'] = line.discount
 
                     conceptos[iline:iline]=[concepto]
                     iline = iline + 1
@@ -179,8 +287,128 @@ class PosOrder(models.Model):
             cron_obj.write({
                 'facturatool_count': last_order_id
             })
-
-
-
         
-        
+    def cron_resend_pos_orders_to_facturatool(self):
+        wsdl = 'http://ws.facturatool.com/index.php?wsdl'
+        client = zeep.Client(wsdl)
+        ft_accounts = {}
+
+        pos_orders = self.env['pos.order'].search([('cfdi_ticket_codigo','!=',''),('cfdi_ticket_state','=','pending'),('cfdi_ticket_calls','<=',5)], limit=30, order='id desc')
+        _logger.debug('===== cron_resend_pos_orders_to_facturatool len(pos_orders) = %r',len(pos_orders))
+
+        for pos_order in pos_orders:
+            if pos_order.company_id.id in ft_accounts:
+                ft_account = ft_accounts[pos_order.company_id.id]
+            else:
+                ft_account = self.env['facturatool.account'].search([('rfc','!=',''),('company_id','=',pos_order.company_id.id)], limit=1)
+                ft_accounts[pos_order.company_id.id] = ft_account
+            
+            #_logger.debug('===== cron_resend_pos_orders_to_facturatool ft_account = %r',ft_account)
+            #_logger.debug('===== cron_resend_pos_orders_to_facturatool pos_order.account_move = %r', pos_order.account_move.cfdi_state)
+            
+            if ft_account.rfc != False and pos_order.account_move.cfdi_state != 'done':
+                _logger.debug('===== cron_resend_pos_orders_to_facturatool pos_order.name = %r',pos_order.name)
+                
+                conceptos = []
+                taxes = []
+                iline = 0
+                itax = 0
+                discount=0
+                for line in pos_order.lines:
+                    _logger.debug('===== cron_resend_pos_orders_to_facturatool line.display_name = %r',line.display_name)
+                    _logger.debug('===== cron_resend_pos_orders_to_facturatool line.tax_ids = %r',line.tax_ids)
+                    _logger.debug('===== cron_resend_pos_orders_to_facturatool line.tax_ids_after_fiscal_position = %r',line.tax_ids_after_fiscal_position)
+                    tax_include_on_price = False
+                    for tax in line.tax_ids:
+                        factor_tax = 1.00
+                        if tax.amount < 0.00:
+                            factor_tax = -1.00
+                        if tax.amount_type=='percent' and tax.type_tax_sat != False:
+                            tax_obj = {
+                                'Nombre': tax.name,
+                                'Tipo': tax.type_tax_sat,
+                                'Impuesto': tax.clave_sat,
+                                'Base': line.price_subtotal,
+                                'TipoFactor': tax.tipo_factor_sat,
+                                'TasaOCuota': (float(tax.amount)/100.00) * factor_tax,
+                                'Importe': float(line.price_subtotal) * (float(tax.amount)/100.00) * factor_tax,
+                                'indexConcepto': iline
+                            }
+                            taxes[itax:itax]=[tax_obj]
+                        itax = itax + 1
+                        if tax.price_include:
+                            tax_include_on_price = True
+                    ValorUnitario = line.price_unit
+                    if tax_include_on_price:
+                        ValorUnitario = float(line.price_subtotal) / float(line.qty)
+                    
+                    concepto = {
+                        'ClaveProdServ': line.product_id.clave_sat.code,
+                        'Cantidad': line.qty,
+                        'Descripcion': line.display_name,
+                        'ClaveUnidad': line.product_uom_id.clave_sat,
+                        'Unidad': line.product_uom_id.name,
+                        'ValorUnitario': ValorUnitario,
+                        'Importe': line.price_subtotal,
+                        'indexConcepto': iline
+                    }
+                    
+                    #if line.discount > 0:
+                    #    discount = discount + line.discount
+                    #    concepto['Descuento'] = line.discount
+
+                    conceptos[iline:iline]=[concepto]
+                    iline = iline + 1
+                
+                #Obtener la forma de pago
+                cfdi_forma_pago = '99'
+                payment_amount = 0.00
+                for payment in pos_order.payment_ids:
+                    if payment.amount > payment_amount:
+                        cfdi_forma_pago = payment.payment_method_id.cfdi_forma_pago.code or cfdi_forma_pago
+                        payment_amount = payment.amount
+                
+                params = {
+                    'Rfc': ft_account.rfc,
+                    'Usuario': ft_account.username,
+                    'Password': ft_account.password,
+                    'Fecha': pos_order.date_order.strftime("%Y-%m-%d %H:%M"),
+                    'Codigo': pos_order.cfdi_ticket_codigo,
+                    'FormaPago': cfdi_forma_pago,
+                    'Conceptos': conceptos,
+                    'Moneda': 'MXN',#pos_order.currency_id.name, hasta que se implemente timbrado para monedas distintas a MXN
+                    'SubTotal': float(pos_order.amount_total) - float(pos_order.amount_tax),
+                    'Total': pos_order.amount_total,
+                    'LugarExpedicion': pos_order.company_id.zip,
+                    'IdExterno': pos_order.name+'_'+str(pos_order.id),
+                }
+                if len(taxes) > 0:
+                    params['Impuestos'] =  taxes
+                
+                _logger.debug('===== cron_resend_pos_orders_to_facturatool crearTicket params = %r',params)
+                result = client.service.crearTicket(params=params)
+                _logger.debug('===== cron_resend_pos_orders_to_facturatool crearTicket result = %r',result)
+                ws_res = json.loads(result)
+                _logger.debug('===== cron_resend_pos_orders_to_facturatool crearTicket ws_res = %r',ws_res)
+
+                if ws_res['success'] == True:
+                    pos_order.write({
+                        'cfdi_ticket_state':'sync',
+                        'cfdi_ticket_id':ws_res['trans_id'],
+                        'cfdi_ticket_calls': pos_order.cfdi_ticket_calls + 1,
+                    })
+                else:
+                    if ws_res['error'] is None:
+                        error = "Servicio temporalmente fuera de servicio"
+                    else:
+                        error = 'Error #' + str(ws_res['errno']) + ': ' + ws_res['error']
+                    pos_order.write({
+                        'cfdi_ticket_calls': pos_order.cfdi_ticket_calls + 1,
+                        'cfdi_ticket_call_error': error
+                    })
+
+            elif pos_order.account_move.cfdi_state == 'done':
+                pos_order.write({
+                    'cfdi_ticket_state':'done',
+                    'cfdi_ticket_calls': 0,
+                })
